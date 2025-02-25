d4585dfba10b4c53f6440c83c7ac05a91c03b4b5
MANOJKUMAR CM <cmmanoj2001@gmail.com>
2025-02-25 13:37:28 +0530

    Added Nesterov_Accelerated_GradientDescent

--- optimizers.py
+++ optimizers.py
@@ -48,6 +48,9 @@ def Momentum_GD(W, b, uW, ub, gradients, learning_rate, beta):
     
     learning_rate: float
         The rate at which the weights and bias is to be updated
+    
+    beta: float
+        momentum coefficient - [0, 1]
     """
     
     L = len(W)
@@ -58,5 +61,42 @@ def Momentum_GD(W, b, uW, ub, gradients, learning_rate, beta):
         
         W[i] = W[i] - learning_rate * uW[i]
         b[i] = b[i] - learning_rate * ub[i]
+        
+    return W, b, uW, ub
+
+def Nesterov_Accelerated_GD(W, b, uW, ub, gradients, learning_rate, beta):
+    """
+    To Update the Weights and bias
+    
+    Parameters:
+    W: dict
+        Dictionary containing weights
+    
+    b: dict
+        Dictionary containing bias vectors
+    
+        uW: dict
+        Dictionary storing exponentially weighted moving averages of past weight gradients (momentum terms)
+    
+    ub: dict
+        Dictionary storing exponentially weighted moving averages of past bias gradients (momentum terms)
+    
+    gradients: dict
+        Dictionary containing gradients with respect to both weights and biases of each layer
+    
+    learning_rate: float
+        The rate at which the weights and bias is to be updated
+    
+    beta: float
+        momentum coefficient - [0, 1]
+    """
+    L= len(W)
+    
+    for i in range(1, L+1):
+        uW[i] = beta * uW[i] - learning_rate * gradients[f"dW{i}"]
+        ub[i] = beta * ub[i] - learning_rate * gradients[f"db{i}"]
+        
+        W[i] = W[i] + beta * uW[i] - learning_rate * gradients[f"dW{i}"]
+        b[i] = b[i] + beta * ub[i] - learning_rate * gradients[f"db{i}"]
         
     return W, b, uW, ub
