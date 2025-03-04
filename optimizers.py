60875e52e4d0e28a62ceb6c28adc5aa558e4083a
MANOJKUMAR CM <cmmanoj2001@gmail.com>
2025-03-04 19:14:29 +0530

    Adding RMSProp

--- optimizers.py
+++ optimizers.py
@@ -37,7 +37,7 @@ def Momentum_GD(W, b, uW, ub, gradients, learning_rate, beta):
     b: dict
         Dictionary containing bias vectors
     
-        uW: dict
+    uW: dict
         Dictionary storing exponentially weighted moving averages of past weight gradients (momentum terms)
     
     ub: dict
@@ -75,7 +75,7 @@ def Nesterov_Accelerated_GD(W, b, uW, ub, gradients, learning_rate, beta):
     b: dict
         Dictionary containing bias vectors
     
-        uW: dict
+    uW: dict
         Dictionary storing exponentially weighted moving averages of past weight gradients (momentum terms)
     
     ub: dict
@@ -98,5 +98,45 @@ def Nesterov_Accelerated_GD(W, b, uW, ub, gradients, learning_rate, beta):
         
         W[i] = W[i] + beta * uW[i] - learning_rate * gradients[f"dW{i}"]
         b[i] = b[i] + beta * ub[i] - learning_rate * gradients[f"db{i}"]
+        
+    return W, b, uW, ub
+
+def RMSProp(W, b, uW, ub, gradients, learning_rate, beta, eps):
+    """
+    To Update the Weights and bias
+    
+    Parameters:
+    W: dict
+        Dictionary containing weights
+    
+    b: dict
+        Dictionary containing bias vectors
+    
+    uW: dict
+        Dictionary storing exponentially weighted moving averages of past weight gradients (momentum terms)
+    
+    ub: dict
+        Dictionary storing exponentially weighted moving averages of past bias gradients (momentum terms)
+    
+    gradients: dict
+        Dictionary containing gradients with respect to both weights and biases of each layer
+    
+    learning_rate: float
+        The rate at which the weights and bias is to be updated
+    
+    beta: float
+        momentum coefficient - [0, 1]
+    
+    eps: float
+        Added to ensure Numerical Stability; prevents divison by zero is uW/ ub - > 0
+    """
+    
+    L = len(W)
+    for i in range(1, L+1):
+        uW[i] = beta * uW[i] + (1 - beta) * np.square(gradients[f"dW{i}"])
+        ub[i] = beta * ub[i] + (1 - beta) * np.square(gradients[f"db{i}"])
+        
+        W[i] = W[i] - (learning_rate/ np.sqrt(uW[i] + eps)) * gradients[f"dW{i}"]
+        b[i] = b[i] - (learning_rate/ np.sqrt(ub[i] + eps)) * gradients[f"db{i}"]
         
     return W, b, uW, ub
