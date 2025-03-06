49a126b16be5d59c597c8e0a9b9cb9ce3bceb0be
MANOJKUMAR CM <cmmanoj2001@gmail.com>
2025-03-06 21:52:59 +0530

    Updated Loss Functions

--- loss.py
+++ loss.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def cross_entropy(Y, y_pred):
-    loss = - np.sum(np.multiply(Y, np.log(y_pred)))/ Y.shape[1] 
-    return loss
+    epsilon = 1e-8
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)  # Avoid log(0)
+    loss = - np.sum(Y * np.log(y_pred))/ Y.shape[1] 
+    return loss 
 
 def Mean_Squared_Error(Y, y_pred):
     loss = 0.5 * np.mean((Y - y_pred)**2)
-    return loss
+    return loss 
+
